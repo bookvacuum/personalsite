4e29a3188fee1f8c5e9c51423d82e3433464363a
Sunwoo Lee <slee20@stanford.edu>
2023-01-29 16:24:59 -0800

    local fonts

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,16 @@
 // components/layout.js
 
+import Link from "next/link";
+
 export default function Layout({ children }) {
   return (
     <>
+    <div className="flex p-4 pt-16 font-sans text-3xl bg-cyan-200">
+        <div className="pl-4 pr-4"><Link href="/"> Home</Link> </div>
+        <div className="pl-4 pr-4"><Link href="/"> Projects</Link></div>
+        <div className="pl-4 pr-4"><Link href="/"> Writings</Link></div>
+        <div className="pl-4 pr-4"><Link href="/"> Contact</Link></div>
+    </div>
       <main>{children}</main>
     </>
   )
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
 import '@/styles/globals.css'
+import Layout from 'components/layout'
+import localFont from '@next/font/local'
+
+const myFont = localFont({ src: './Quantico-Regular.ttf' })
+
+
+
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={myFont.className}>
+     <Layout>
+  <Component {...pageProps} />
+  </Layout>
+  </main>
+  )
 }
