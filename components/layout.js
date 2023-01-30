a708d44c5a2b928c745d12e55002d6a7d9adca81
Sunwoo Lee <slee20@stanford.edu>
2023-01-30 09:39:30 -0800

    added pic and main body

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Layout({ children }) {
   return (
     <>
-    <div className="p-4 pt-10 font-sans">
+    <div className="p-4 pt-10 pb-8 font-sans">
       <div className='flex float-right sm:space-x-10'> 
         <div className="pl-2 pr-2"><Link href="/"> Home</Link> </div>
         <div className="pl-2 pr-2"><Link href="/"> Projects</Link></div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-
+import Image from 'next/image'
+import profile from '/public/profilepic.jpg'
 
 export default function Home() {
   return (
@@ -10,8 +11,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <h1 className='font-sans text-5xl'>hi</h1> 
-  
+      
+      <Image alt="image of me" className='pb-7' src={profile}></Image>
+
+     <div className='pl-5 pr-5 text-center'> 
+        <h1 className='font-sans text-4xl italic'>Hi! I&apos;m Sunwoo</h1> 
+        <p className='font-sans'>My name means giver of valuables</p> 
+
+      </div>
     </>
   )
 }
