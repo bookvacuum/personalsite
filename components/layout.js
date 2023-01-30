7032e47e749b5c8b63a093399d8b4b1bd2ec9463
Sunwoo Lee <slee20@stanford.edu>
2023-01-29 16:53:46 -0800

    mobile centric navbar

--- components/layout.js
+++ components/layout.js
@@ -5,11 +5,13 @@ import Link from "next/link";
 export default function Layout({ children }) {
   return (
     <>
-    <div className="flex p-4 pt-16 font-sans text-3xl bg-cyan-200">
-        <div className="pl-4 pr-4"><Link href="/"> Home</Link> </div>
-        <div className="pl-4 pr-4"><Link href="/"> Projects</Link></div>
-        <div className="pl-4 pr-4"><Link href="/"> Writings</Link></div>
-        <div className="pl-4 pr-4"><Link href="/"> Contact</Link></div>
+    <div className="p-4 pt-10 font-sans">
+      <div className='flex float-right sm:space-x-10'> 
+        <div className="pl-2 pr-2"><Link href="/"> Home</Link> </div>
+        <div className="pl-2 pr-2"><Link href="/"> Projects</Link></div>
+        <div className="pl-2 pr-2"><Link href="/"> Writings</Link></div>
+        <div className="pl-2 pr-2"><Link href="/"> Contact</Link></div>
+      </div>
     </div>
       <main>{children}</main>
     </>
