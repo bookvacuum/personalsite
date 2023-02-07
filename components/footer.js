bf73f574aa2ee58270b50cf6df9fac733836dcaa
Sunwoo Lee <slee20@stanford.edu>
2023-02-06 20:34:24 -0800

    finished footer

--- components/footer.js
+++ components/footer.js
@@ -1,18 +1,15 @@
-import Link from 'next/link'
+import github from '/public/github-mark.png';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Footer() {
     
 
     return(
-        <div className="h-32 text-center bg-purple-300 sm:h-48">
-               <div className='pt-5'>made with NextJS</div>
-            <div className='flex justify-center pt-10 mx-auto'>
-            <a href="https://twitter.com/SunwooLee__?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @SunwooLee__</a>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="navy" class="bi bi-linkedin" viewBox="0 0 16 16">
-  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-</svg>
-            </div>
-            </div>
+        <div className="justify-center h-32 text-center bg-purple-300 sm:h-48">
+            <Link href='https://github.com/bookvacuum/personalsite' className='relative text-sm rounded top-12 sm:top-20'>The code for this website lives here<Image className="inline-flex ml-1" src={github} alt="github" width="20" height="20" /></Link>
+        </div>
+
     )
 }
 
